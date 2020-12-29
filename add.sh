3b41a0ef0137474c2ce81e91e242fd1aa3a25e94
sai <sswathi6006@gmail.com>
2020-12-28 22:18:01 -0600

    1st commit

---
+++ add.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Adding 2 numbers
+
+echo "addition of X+Y"
+echo "Enter X"
+read X
+echo "Enter Y"
+read Y
+echo "X+Y = $X+$Y = $[ X+Y ]"
---
+++ for_loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# for loop
+
+for i in 1 2 3 4 5
+do
+echo $i
+done
